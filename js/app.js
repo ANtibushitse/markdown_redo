beeef16cf893fe62c26e61e39c2a94386c011e94
Ntibushitse Arsène <ntibushitse.arsene@gmail.com>
2016-11-12 15:31:20 +0100

    nital commit

---
+++ js/app.js
@@ -0,0 +1,20 @@
+(function(){
+	console.log("start here"); 
+	"use strict"
+	var app ={
+		url1:null,  
+
+		init:function(){
+			app.url1(); 
+
+		},
+		url1:function(){
+
+
+		}, 
+
+	}
+
+
+
+})(); 
